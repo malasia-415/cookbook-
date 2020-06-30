4a389d4a905d8175b07cc15c61b71a3e652b40d0
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-06-29 20:54:03 -0700

    edits

--- Comments pg/index.js
+++ Comments pg/index.js
@@ -1,72 +1,73 @@
-let store = [];
-function recipes() {
-  $('#record-recipe').submit(event => { 
-    event.preventDefault()
-  })
+// let store = [];
+// function recipes() {
+//   $('#record-recipe').submit(event => { 
+//     event.preventDefault()
+//   })
 
- eventListeners() {
-    this.recipes = document.getElementById('recipes')
-    this.newForm.addEventListener('submit', this.getForm)
-  }
+//  eventListeners() {
+//     this.recipes = document.getElementById('recipes')
+//     this.newForm.addEventListener('submit', this.getForm)
+//   }
 
-  function newForm() {
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://cookbook-git-master.malasia-415.vercel.app/";
-  }
-    fetch(proxyurl)
-    .then(response => response.json())
+//   function newForm() {
+//     return
+//     const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//     const url = "https://cookbook-git-master.malasia-415.vercel.app/";
+//   }
+//     fetch(proxyurl)
+//     .then(response => response.json())
 
-    .then (responeJson => {
-      let newForm = responsJson.results
-      let getForm = [];
+//     .then (responeJson => {
+//       let newForm = responsJson.results
+//       let getForm = [];
 
 
-      handleSubmit(event) {
-        event.preventDefault();
-        const newForm = event.target.getForm.value;
-        this.getForm(newForm);
-        this.props.history.goBack();
-      }
-      console.log(newForm);
-      this.newForm;
-      this.props.history.push(' ');
+//       handleSubmit(event) {
+//         event.preventDefault();
+//         const newForm = event.target.getForm.value;
+//         this.getForm(newForm);
+//         this.props.history.goBack();
+//       }
+//       console.log(newForm);
+//       this.newForm;
+//       this.props.history.push(' ');
   
-      .then((response) => {
-        if (response.status === 400) {
-          throw new Error(response.error);
-        }
-        return response.json();
-    })
+//       .then((response) => {
+//         if (response.status === 400) {
+//           throw new Error(response.error);
+//         }
+//         return response.json();
+//     })
   
   
-    .then(() => {
-      // set in local storage
-      window.localStorage.getForm("token", user.authToken);
-      // update state
-      this.setState({ isAuth: true, username: user.username });
-      console.log(user.username, "is logged in!");
-      // after login redirect to to journal
-      this.props.history.push("/journal-setup");
-      // CHANGE THIS BACK TO JOURNAL-SETUP LATER
-      // this.props.history.push("/dashboard");
-    })
+//     .then(() => {
+//       // set in local storage
+//       window.localStorage.getForm("token", user.authToken);
+//       // update state
+//       this.setState({ isAuth: true, username: user.username });
+//       console.log(user.username, "is logged in!");
+//       // after login redirect to to journal
+//       this.props.history.push("/journal-setup");
+//       // CHANGE THIS BACK TO JOURNAL-SETUP LATER
+//       // this.props.history.push("/dashboard");
+//     })
   
 
-      newForm.forEach(newForm => {
-        getForm += `<div class="recipes"></div>`
-      })
+//       newForm.forEach(newForm => {
+//         getForm += `<div class="recipes"></div>`
+//       })
 
-      $("#newForm").append();
-    })
+//       $("#newForm").append();
+//     })
 
     
-  var toInsert = document.createElement("div");
-    toInsert.innerHTML = "text to insert";
+//   var toInsert = document.createElement("div");
+//     toInsert.innerHTML = "text to insert";
 
-  updateForm(event) {
-    const newForm = event.target.value;
-    this.context.updateForm(newForm);
-  }
+//   updateForm(event) {
+//     const newForm = event.target.value;
+//     this.context.updateForm(newForm);
+//   }
 
 
 
--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+let store = [];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function recipes() {
+  $('#record-recipe').submit(event => { 
+    event.preventDefault()
+  })
 
-export default App;
+ function eventListeners() {
+    this.recipes = document.getElementById('recipes')
+    this.newForm.addEventListener('submit', this.getForm)
+  }
+
+  function newForm() {
+    return
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://cookbook-git-master.malasia-415.vercel.app/";
+  }
+    fetch(proxyurl)
+    .then(response => response.json())
+
+    .then (responeJson => {
+      let newForm = responsJson.results
+      let getForm = [];
+
+
+      function handleSubmit(event) {
+        event.preventDefault();
+        const newForm = event.target.getForm.value;
+        this.getForm(newForm);
+        this.props.history.goBack();
+  
+    //   .then((response) => {
+    //     if (response.status === 400) {
+    //       throw new Error(response.error);
+    //     }
+    //     return response.json();
+    // })
+  }
+  console.log(newForm);
+      this.newForm;
+      this.props.history.push(' ');
+  
+  
+    .then(() => {
+      // set in local storage
+      window.localStorage.getForm("token", getForm.authToken);
+      // update state
+      this.setState({ isAuth: true, name: getForm.name });
+      console.log(getForm.name, "is logged in!");
+      // after login redirect to to journal
+      this.props.history.push("/recod-recipe");
+      // CHANGE THIS BACK TO JOURNAL-SETUP LATER
+      // this.props.history.push("/dashboard");
+    })
+  
+
+      newForm.forEach(newForm => {
+        getForm += `<div class="recipes"></div>`
+      })
+
+      $("#newForm").append();
+    })
+
+    
+  var toInsert = document.createElement("div");
+    toInsert.innerHTML = "text to insert";
+
+  updateForm(event) {
+    const newForm = event.target.value;
+    this.context.updateForm(newForm);
+  }
+
+
+
